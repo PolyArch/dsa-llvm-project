1c46f6b1e1de979e4c51b6124c369144985ce6e0
Zhengrong Wang <seanyukigeek@gmail.com>
2020-09-05 15:57:59 -0700

    [misc] Disable omptarget in setup script.

--- setup.llvm.debug.sh
+++ setup.llvm.debug.sh
@@ -1,10 +1,11 @@
-cmake -G "Ninja" \
+cmake -G "Unix Makefiles" \
 -DLLVM_TARGETS_TO_BUILD="X86" \
 -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="RISCV" \
 -DLLVM_ENABLE_PROJECTS="clang;libunwind;lld;openmp" \
 -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_INSTALL_PREFIX=../install-debug \
--DLLVM_BINUTILS_INCDIR=~/Documents/binutils/include \
+-DLLVM_BINUTILS_INCDIR=~/binutils/include \
 -DBUILD_SHARED_LIBS=ON \
+-DOPENMP_ENABLE_LIBOMPTARGET=OFF \
 ../llvm \
 
--- setup.omp.sh
+++ setup.omp.sh
@@ -2,5 +2,6 @@ cmake \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=../install-release \
 -DLIBOMP_ENABLE_SHARED=False \
+-DOPENMP_ENABLE_LIBOMPTARGET=OFF \
 ../openmp \
 
